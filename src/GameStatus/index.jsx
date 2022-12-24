af80be1a8cf21b4045350a99233a56c0478ccf3d
taherfa <t.ankleshwaria@gmail.com>
2022-12-24 05:23:46 -0700

    make outcome pop

--- src/GameStatus/index.jsx
+++ src/GameStatus/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const GameStatus = ({ outcome }) => {
   return (
     <div>
-      <h1>{!outcome ? `Checkers` : outcome === `computer` ? `${outcome} wins!`: `You win!`}</h1>
+      {!outcome ? (
+        <h2>Checkers</h2>
+      ) : (
+        <h1>{outcome === `computer` ? `${outcome} wins!` : `You win!`}</h1>
+      )}
     </div>
   );
 };
